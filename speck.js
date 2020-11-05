fcb880e9d22b3c5352758781e6077d335ef36aae
Connor Ward <cward.contrib@fastmail.com>
2020-11-05 21:37:49 +0000

    Working prototype

---
+++ speck.js
@@ -0,0 +1,101 @@
+/**
+ * Main class.
+ */
+class Program {
+
+    /**
+     * Create the program.
+     */
+    constructor() {
+        this.map = this._initMap();
+        this.mapProvider = new GeoSearch.OpenStreetMapProvider();
+        this.markers = [];
+        this.table = document.getElementById("addresses");
+    }
+
+
+    /**
+     * Creates the correct number of table entries.
+     */
+    makeTable() {
+        const addressCount = document.getElementById("address-count").valueAsNumber;
+
+        // Create the correct amount of rows. If there are too many then remove
+        // the right amount instead.
+        let nRows = this.table.rows.length;
+        while (nRows != addressCount + 1) {
+            if (nRows > addressCount + 1) {
+                this.table.deleteRow(-1);
+                nRows--;
+            } else {
+                const row = this.table.insertRow(-1);
+
+                row.insertCell(0).innerHTML = nRows;
+                row.insertCell(1).innerHTML = "<input type='text'>"
+                nRows++;
+            }
+        }
+    }
+
+
+    /**
+     * Update the map.
+     */
+    async updateMarkers() {
+        // Remove all previous markers.
+        for (const marker of this.markers) 
+            marker.remove();
+        this.markers = [];
+
+        // Process the table entries and add markers to the map.
+        for (const row of this.table.rows) {
+            // Skip first (heading) row.
+            if (row.rowIndex == 0) continue;
+
+            // Search for the address.
+            const inputField = row.cells[1].children[0];
+            const address = inputField.value;
+
+            // Skip empty fields.
+            if (address === "") continue;
+
+            // Search for the address and take the first result.
+            const results = await this.mapProvider.search({ query: address });
+
+            // Highlight in red if no results found.
+            if (results.length == 0) {
+                inputField.style.background = "red";
+                continue;
+            }
+
+            const result = results[0];
+
+            // Create the marker.
+            const marker = L.marker([result.y, result.x])
+                .addTo(this.map)
+                .bindPopup(address);
+            this.markers.push(marker);
+        }
+    }
+
+
+    /**
+     * Initialise the map.
+     */
+    _initMap() {
+        // Create the map and add tiling to it.
+        let map = L.map("map").setView([51.319129, -0.559470], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: "pk.eyJ1IjoiY2p3YXJkIiwiYSI6ImNraDU1OWFnZjAybTEycXBlcHRnY3pyZTMifQ.v6TcrIOVzT9IbQyMxBudcQ"
+        }).addTo(map);
+
+        return map;
+    }
+}
+
+var program = new Program();
